4fd6f321fc6508fb91e0c39fa84b056ad0c123bb
Santiago Roca <santiroca88@gmail.com>
2023-03-08 14:09:09 -0300

    Modifico README

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Item from "./Item";
 import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () => {
